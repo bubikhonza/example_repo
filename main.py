cdb35781514c2e9d4dd712bc96dbdc45dfbe5adc
Jan Bubik <bubikhonza@Jan--MacBook-Air.local>
2026-03-10 18:13:03 +0100

    some other changes

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-x = 5
+
+
+def print_status(funkce):
+    def wrapper(*args, **kwargs):
+        print("Spouštím funkci...")
+        vysledek = funkce(*args, **kwargs)
+        print("Hotovo.")
+        return vysledek
+    return wrapper
+
+@print_status
+def sum(a, b):
+    print("Funkce bezi")
+    return a + b
+
+res = sum(1,2)
+print(res)
